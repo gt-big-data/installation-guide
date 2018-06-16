7e78db1d8d5120935d7423bdb6d812ed6dcad437
Rahul Zhade <zhade3@github.com>
2018-06-16 13:04:43 -0700

    Added shell scripts to download/ run tools

---
+++ scripts/linux.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt install git-all
+sudo apt install mongodb-org
+sudo apt-get install python3.6
---
+++ scripts/windows.bat
@@ -0,0 +1,5 @@
+:: For starting up MongoDB
+
+md \data\db
+C:\Program Files\MongoDB\Server\4.0\bin\mongod.exe
+:: Should see this if everything works fine: [initandlisten] waiting for connections on port 27017
